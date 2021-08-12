2ea8ab92e26c7ac2192414eb5d4e439b816450aa
Walter Barreiro <walterbarreiro97@gmail.com>
2021-08-12 08:46:52 -0300

    Entrega 1 ejercicio 2

--- js/products.js
+++ js/products.js
@@ -1,6 +1,37 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+// let currentProductArray = [];
 
+function showProductsList(currentProductsArray){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let product = currentProductsArray[i];
+            htmlContentToAppend += `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` vendidos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                        <p class="mb-1"> ${product.currency} ${product.cost}</p>
+                    </div>
+                </div>
+            </a>
+            `
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            showProductsList(resultObj.data);
+        }
+    });
 });
