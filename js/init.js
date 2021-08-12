3a2f390c5cd5fc61a63aae5ad82c6e8bfd9ac42b
Walter Barreiro <walterbarreiro97@gmail.com>
2021-08-12 10:14:41 -0300

    Entrega 1 ejercicio 3

--- js/init.js
+++ js/init.js
@@ -7,8 +7,14 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+document.getElementById("btn-goToHome").onclick = function () {
+    location.href = "home.html";
+};
+
+
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
+  
 }
 
 var hideSpinner = function(){
